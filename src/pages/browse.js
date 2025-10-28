45cb7555e15832a1d48e9591722c4012586ab9d9
WonUpped <robinson.colin@outlook.com>
2025-10-28 00:46:43 -0400

    Added temporary working pages to all routes/tabs. Navbar tab buttons now route properly without crashing.

---
+++ src/pages/browse.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Browse.css"
+
+function Browse() {
+  return(
+    <div className="Browse">
+      <header className="Browse-header">
+        <h1>Browse SHOPNAO</h1>
+        /* */
+      </header>
+    </div>
+  
+  );
+}
+
+
+
+export default Browse;
