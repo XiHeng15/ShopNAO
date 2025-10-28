c564cb94b3628009b483a0536d345617424237d4
WonUpped <robinson.colin@outlook.com>
2025-10-28 00:24:53 -0400

    Added tab buttons, added  navigation functionaility- still a WIP, currently crashes due to unknown error. Fixed a bunch of navbar rendering issues and improved design

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Browse from './pages/browse';
 import Cart from './pages/cart';
-import Navbar from './components/navbar';
+import About from './pages/about';
+import Navbar from './components/Navbar';
 import "./App.css"
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path ="/browse" element={<Browse/>}/>
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/" element={<Home />}/>
+        <Route path="/browse" element={<Browse/>}/>
+        <Route path="/cart" element={<Cart />}/>
+        <Route path="/about" element={<About />}/>
       </Routes>
     </Router>
   );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,23 @@
 import React from "react"
+import { NavLink } from "react-router-dom";
 import logo from "../logo.svg";
-import "./navbar.css"
+import "./Navbar.css";
 
 export default function Navbar(){
     return(
-        <div className="navbar">
-            <img src={logo} alt="Logo" className="logo"/>
-            <h1 className="title">ShopNAO | Only the best.</h1>
-        </div>
+        <nav className="navbar">
+            <div className="navbar-left">
+                <img src={logo} alt="Logo" className="logo"/>
+                <h1 className="title">ShopNAO | Only the best.</h1>
+            </div>
+            <div className="navbar-tabs">
+                <NavLink to="/" className="tab" end>Home</NavLink>
+                <NavLink to="/browse" className="tab">Browse</NavLink>
+                <NavLink to="/cart" className="tab">Cart</NavLink>
+                <NavLink to="/about" className="tab">About</NavLink>
+            </div>
+        </nav>
     );
     
 }
+
