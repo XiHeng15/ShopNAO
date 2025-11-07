fd75903256dc3b97aa318cd083243812a4860512
Koz <mk272004@gmail.com>
2025-11-07 15:22:55 -0500

    added comments to product_card js

--- src/components/product_card.js
+++ src/components/product_card.js
@@ -1,37 +1,43 @@
 import "./product_card.css";
 import { useState } from "react";
 
+//product-card component that shows a product (product info, image, price)
 export default function ProductCard({ item_img_url, message, price }) {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(1); //tracks the item quantity
 
   return (
-    <div className="Card">
+    /* displays what is shown on the product screen */
+    <div className="Card"> 
       <h1>{message}</h1>
       <Item item_img_url={item_img_url} />
       <h3>${price * count}</h3>
       <p>Quantity: {count}</p>
       <Counter count={count} setCount={setCount} />
       <Button />
-    </div>
-  );
+    </div> 
+  ); 
 }
 
+//Runs when someone presses the "Add to cart" button 
 function handleClick() {
-  alert("Item(s) are NAO purchased :D");
+  alert("Item(s) are NAO purchased :D"); //temporary alert msg when clicked
 }
 
+//Button to add item(s) to the cart
 function Button() {
   return (
     <div>
-      <button onClick={handleClick}> Add to cart NAO </button>
+      <button onClick={handleClick}> Add to cart NAO </button> 
     </div>
   );
 }
 
+//product image
 function Item({ item_img_url }) {
   return <img src={item_img_url} alt="" />;
 }
 
+//Product quantity counter based on + or - button clicks
 function Counter({ count, setCount }) {
   return (
     <div>
