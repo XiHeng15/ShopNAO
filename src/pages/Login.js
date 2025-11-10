d4bf580cc9dcaf9afe57312658c2a32a66ee7f2a
XiHeng15 <146135850+XiHeng15@users.noreply.github.com>
2025-11-09 19:14:02 -0500

    should be updated with evertyhign

---
+++ src/pages/Login.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import "./Login.css"
+
+export default function App() {
+  return (
+    <div className="Card">
+      <header>
+        <Login />
+      </header>
+    </div>
+  );
+}
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleUsernameChange(e) {
+    setUsername(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  return (
+    <div className="Card">
+      <h2>Log in to Shop NAO</h2>
+      <label>
+        Username: <input value={username} onChange={handleUsernameChange} />
+      </label>
+      <label>
+        Password: <input value={password} onChange={handlePasswordChange} />
+      </label>
+      <Button />
+    </div>
+  );
+}
+
+function Button() {
+  return (
+    <div>
+      <button onClick={handleClick}> Sign In </button>
+    </div>
+  );
+}
+
+function handleClick() {
+  alert(
+    "Hopefully we can ping the database to verify for correct usernames/passwords"
+  ); //temporary alert msg when clicked
+}
