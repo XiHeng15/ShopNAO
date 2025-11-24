2a8959567e5624a0e0deaa4ebe7ebb78f17108ed
WonUpped <robinson.colin@outlook.com>
2025-11-24 14:11:16 -0500

    edited product page to actually work, now when a product in browse is clicked it sends to a page specifically for the product eg http://localhost:3000/product/<id>

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -55,6 +55,27 @@ router.get("/business", authenticateBusiness, async (req, res) => {
   }
 });
 
+// GET /api/products/:id - Get one product by ID (MUST BE BELOW /api/products/business!!!!)
+const mongoose = require("mongoose");
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Invalid product ID" });
+  }
+
+  try {
+    const product = await Product.findById(id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
 
 // POST /api/products/add
 router.post('/add', authenticateBusiness, upload.single('image'), async (req, res) => {
