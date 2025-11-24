84c2865f06f04f7acf38c038cd13e5098f376e35
WonUpped <robinson.colin@outlook.com>
2025-11-23 19:51:01 -0500

    Minor fix, login button changes to greeting after logging in!

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,27 +1,36 @@
-import React from "react"
 import { NavLink } from "react-router-dom";
 import logo from "../logo.svg";
 import "./Navbar.css";
 
-const role = localStorage.getItem("role");
+export default function Navbar() {
+  const name = localStorage.getItem("userName");  // get user name
+  const role = localStorage.getItem("role");  // for showing business tab
 
-export default function Navbar(){
-    return(
-        <nav className="navbar">
-            <div className="navbar-left">
-                <img src={logo} alt="Logo" className="logo"/>
-                <h1 className="title">ShopNAO | Only the best.</h1>
-            </div>
-            <div className="navbar-tabs">
-                <NavLink to="/" className="tab" end>Home</NavLink>
-                <NavLink to="/browse" className="tab">Browse</NavLink>
-                {role === "business" && <NavLink to="/business" className ="tab">Business</NavLink>}
-                <NavLink to="/cart" className="tab">Cart</NavLink>
-                <NavLink to="/about" className="tab">About</NavLink>
-                <NavLink to="/login" className="tab">Login</NavLink>
-            </div>
-        </nav> 
-    );
-    
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="Logo" className="logo"/>
+        <h1 className="title">ShopNAO | Only the best.</h1>
+      </div>
+
+      <div className="navbar-tabs">
+        <NavLink to="/" className="tab" end>Home</NavLink>
+        <NavLink to="/browse" className="tab">Browse</NavLink>
+        
+        {role === "business" && (
+        <NavLink to="/BusinessDashboard" className="tab">
+            Business Dashboard
+        </NavLink>
+        )}
+        
+        <NavLink to="/cart" className="tab">Cart</NavLink>
+        <NavLink to="/about" className="tab">About</NavLink>
+        {!name ? (
+          <NavLink to="/login" className="tab">Login</NavLink>
+        ) : (
+          <span className="tab">Hello, {name}!</span>
+        )}
+      </div>
+    </nav>
+  );
 }
-
