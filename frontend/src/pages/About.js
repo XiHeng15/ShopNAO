36852e0470f99ceacb4f3766fd07136b1e53e2b6
PlantEnjoyer69 <abimrah1234@gmail.com>
2025-12-01 15:48:11 -0500

    Edited css of about page, bare bones rn

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./About.css"
+import logo from "../Logo.svg";
 
 function About() {
   return(
     <div className="About">
       <header className="About-header">
         <h1>About SHOPNAO</h1>
+        <img src={logo} alt="Logo" className="logo"/>
         <p>
           SHOPNAO is your one-stop online shop for all your needs. We offer a wide range of products from hammers to hammers and even hammers!
         </p>
