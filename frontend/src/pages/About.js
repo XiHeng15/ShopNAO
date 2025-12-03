19e2cfc4e140ebc92fa90abfd11ea29108c367fe
Plant Enjoyer <abimrah1234@gmail.com>
2025-12-02 19:46:18 -0500

    more css tweaking

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -8,16 +8,26 @@ function About() {
       <header className="About-header">
         
         <h1>About SHOPNAO</h1>
-        <div className="about-box">        
+        <div className="about-box1">        
           <img src={logo} alt="Logo" className="logo"/>
-        <p>
-          SHOPNAO is your one-stop online shop for all your needs. We offer a wide range of products from hammers to hammers and even hammers!
-        </p></div>
+          <p>
+            SHOPNAO is an E-Commerce website built by people who are REALLY passionate about hammers but dont know anything about them
+          </p>
+        </div>  
+          
+        <div className="about-box1">        
+          <img src={logo} alt="Logo" className="logo"/>
+          <p>
+            This website was developed using frameworks such as React, MongoDB and many more (be sure to read our "READ ME" for specific module credits)
+          </p>
+        </div>
         
       </header>
     </div>
   
   );
 }
+
+
 
 export default About;
