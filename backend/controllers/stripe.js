e73e73fd8b54f4fb5fe8aa5cddd0bdc6702d8696
WonUpped <robinson.colin@outlook.com>
2025-12-04 00:54:05 -0500

    forgot a comment

--- backend/controllers/stripe.js
+++ backend/controllers/stripe.js
@@ -1,3 +1,3 @@
-require("dotenv").config();
+require("dotenv").config(); //just a basic controller for stripe
 const Stripe = require("stripe");
 module.exports = Stripe(process.env.STRIPE_SECRET_KEY);
